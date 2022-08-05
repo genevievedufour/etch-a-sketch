0f05194dde87ac67f1c4b6280c00fb2b990f4756
Genevieve Dufour <genevieve.dufour.8@gmail.com>
2022-08-04 22:09:36 -0400

    Add slider, reset button, remove cell

--- javascript.js
+++ javascript.js
@@ -1,23 +1,54 @@
 let gridContainer = document.querySelector(".grid-container");
 
-let gridSize = 64;
-
 let root = document.querySelector(':root');
-root.style.setProperty('--gridSize', gridSize);
 
+let gridSizeSlider = document.querySelector("#grid-size-slider");
+let gridSizeLabel= document.querySelector("#grid-size-label");
+let resetButton = document.querySelector("#reset-button");
 
-function onSquareHover(e){
+resetGrid();
+
+function setGridSize(newGridSize){
+    gridContainer.innerHTML = "";
+    root.style.setProperty('--gridSize', newGridSize);
+    gridSizeSlider.value = newGridSize;
+    gridSizeLabel.textContent = newGridSize + " x " + newGridSize;
+    drawGrid(newGridSize);
+}
+
+function onSquareHover(e){    
+    if(e.buttons === 4){
+        this.classList.remove('cell-selected');
+    }
+    if(e.buttons !== 1)
+        return;
+
     this.classList.add('cell-selected');
 }
 
-for(let i = 0; i < gridSize; i++){
-    for(let j = 0; j < gridSize; j++){
-        let square = document.createElement("div");
-        square.classList.add('cell');
-
-        square.addEventListener('mouseover', onSquareHover);
-
-        gridContainer.appendChild(square);
-    }   
+function onSliderChange(e){
+    setGridSize(this.value);
 }
 
+function drawGrid(gridSize){
+    for(let i = 0; i < gridSize; i++){
+        for(let j = 0; j < gridSize; j++){
+            let square = document.createElement("div");
+            square.classList.add('cell');
+    
+            square.addEventListener('mouseover', onSquareHover);
+            square.addEventListener('mousedown', onSquareHover);
+    
+            gridContainer.appendChild(square);
+        }   
+    }
+    
+}
+
+function resetGrid(){
+    setGridSize(16);
+}
+
+resetButton.addEventListener('click', resetGrid);
+
+gridSizeSlider.onchange = onSliderChange;
